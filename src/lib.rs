b954a24a973a3fdc7ea2f19a0e6808ba6aacf871
Ulysse Carion <ulysse@segment.com>
2019-06-27 12:19:33 -0700

    Add module-level docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! A Rust client to Segment's tracking API.
+//!
+//! For more on what Segment is, refer to [Segment's
+//! documentation](https://segment.com/docs/).
+
 pub mod batcher;
 pub mod client;
 pub mod errors;
